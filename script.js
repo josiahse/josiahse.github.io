e484837156b44654df9a038ed4e4c1adb524e64a
josiahse <josiah@josiahse.com>
2021-10-11 15:40:51 -0600

    added slideshow for desktop design

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ $('.fa-bars').on('click', function(event){
     $('.nav-box').toggleClass('show-nav')
     $('main').toggleClass('move-main')
 });
+
+let slideIndex = 1;
+
+//dir will either be +1 or -1 depending on whether "next" or "prev" is clicked
+const switchSlide = (dir) => {
+    console.log(slideIndex)
+    $(`.proj${slideIndex}`).toggleClass('show-slide');
+    slideIndex += dir;
+    console.log(slideIndex);
+    //enable proper looping of slides
+    if (slideIndex === 6){
+        slideIndex = 1;
+    } else if (slideIndex === 0) {
+        slideIndex = 5;
+    }
+    $(`.proj${slideIndex}`).toggleClass('show-slide');
+}
+
+$('.next').on('click', function(event) {
+    event.preventDefault;
+    switchSlide(1);
+});
+
+$('.prev').on('click', function (event) {
+    event.preventDefault;
+	switchSlide(-1);
+});
