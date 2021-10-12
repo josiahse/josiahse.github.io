c8eae7ff2dff3cffce38d464c78f1fb22f94803f
josiahse <josiah@josiahse.com>
2021-10-12 17:40:36 -0600

    added images and names of 4 projects to portfolio, need to link still

--- script.js
+++ script.js
@@ -8,15 +8,13 @@ let slideIndex = 1;
 
 //dir will either be +1 or -1 depending on whether "next" or "prev" is clicked
 const switchSlide = (dir) => {
-    console.log(slideIndex)
     $(`.proj${slideIndex}`).toggleClass('show-slide');
     slideIndex += dir;
-    console.log(slideIndex);
     //enable proper looping of slides
-    if (slideIndex === 6){
+    if (slideIndex === 5){
         slideIndex = 1;
     } else if (slideIndex === 0) {
-        slideIndex = 5;
+        slideIndex = 4;
     }
     $(`.proj${slideIndex}`).toggleClass('show-slide');
 }
