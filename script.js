1537cef6aa36f06f3098c7da4670c2d2cc3e8427
josiahse <josiah@josiahse.com>
2021-10-13 15:38:15 -0600

    added some comments and general tidying

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-$('.fa-bars').on('click', function(event){
-    event.preventDefault;
-    $('.nav-box').toggleClass('show-nav')
-    $('main').toggleClass('move-main')
+$('.fa-bars').on('click', function (event) {
+	event.preventDefault;
+	$('.nav-box').toggleClass('show-nav');
+	$('main').toggleClass('move-main');
 });
 
 let slideIndex = 1;
 
 //dir will either be +1 or -1 depending on whether "next" or "prev" is clicked
 const switchSlide = (dir) => {
-    $(`.proj${slideIndex}`).toggleClass('show-slide');
-    slideIndex += dir;
-    //enable proper looping of slides
-    lastSlideIndex = parseInt($('.projects').find('>li:last-of-type').attr('class').at(-1));
-    if (slideIndex === lastSlideIndex + 1){
-        slideIndex = 1;
-    } else if (slideIndex === 0) {
-        slideIndex = lastSlideIndex;
-    }
-    $(`.proj${slideIndex}`).toggleClass('show-slide');
-}
+	//hide the current slide
+	$(`.proj${slideIndex}`).toggleClass('show-slide');
+	slideIndex += dir;
+	//automatically detect how many projects are listed and properly cycle through them
+	lastSlideIndex = parseInt(
+		$('.projects').find('>li:last-of-type').attr('class').at(-1)
+	);
+	if (slideIndex === lastSlideIndex + 1) {
+		slideIndex = 1;
+	} else if (slideIndex === 0) {
+		slideIndex = lastSlideIndex;
+	}
+	//show the next slide
+	$(`.proj${slideIndex}`).toggleClass('show-slide');
+};
 
-$('.next').on('click', function(event) {
-    event.preventDefault;
-    switchSlide(1);
+$('.next').on('click', function (event) {
+	event.preventDefault;
+	switchSlide(1);
 });
 
 $('.prev').on('click', function (event) {
-    event.preventDefault;
+	event.preventDefault;
 	switchSlide(-1);
 });
